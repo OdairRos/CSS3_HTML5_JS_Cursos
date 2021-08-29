1eed42c729c9dce6c8159d2373331416b657ea22
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-08-28 23:24:01 -0300

    novas aulas

---
+++ HTML/[RocketSeat] Html para Astronautas/Pilotando a DOM/Aulas/Aula 007/QualUsar.js
@@ -0,0 +1,10 @@
+/*
+
+    QUAL USAR?
+
+    Getelementbyid (element)
+    getelementbyclass (html collection)
+    getelementbytagname (html collection)
+    queryselector (element)
+    queryselectorall (node list)
+*/
---
+++ HTML/[RocketSeat] Html para Astronautas/Pilotando a DOM/Aulas/Aula 008/script.js
@@ -0,0 +1,4 @@
+const el = document.querySelector('h1')
+
+el.textContent += ' odair'
+console.log(el.textContent)
