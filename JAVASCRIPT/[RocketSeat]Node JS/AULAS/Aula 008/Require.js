83c1455e7f79a9c72254658386f40912dcfb2fee
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-10-26 16:02:34 -0300

    new arquive

---
+++ JAVASCRIPT/[RocketSeat]Node JS/AULAS/Aula 008/Require.js
@@ -0,0 +1,5 @@
+const path = require('path');
+// require server para utilizar modulos
+
+
+console.log(path.basename(__filename))
---
+++ JAVASCRIPT/[RocketSeat]Node JS/AULAS/Aula 009/Node.js
@@ -0,0 +1,3 @@
+const MyModule = require('./Export')
+
+console.log(MyModule)
