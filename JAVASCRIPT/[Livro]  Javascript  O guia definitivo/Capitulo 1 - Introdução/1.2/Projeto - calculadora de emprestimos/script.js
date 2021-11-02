087601d433d882f5a6cd58330b1d0430672c5f15
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-11-02 18:29:42 -0300

    new

--- JAVASCRIPT/[Livro]  Javascript  O guia definitivo/Capitulo 1 - Introdução/1.2/Projeto - calculadora de emprestimos/script.js
+++ JAVASCRIPT/[Livro]  Javascript  O guia definitivo/Capitulo 1 - Introdução/1.2/Projeto - calculadora de emprestimos/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require('math')
 function calcular(){
     var amount = document.getElementById('quantidade')
     var apr = document.getElementById('apr')
@@ -14,7 +15,7 @@ function calcular(){
     var interest = parseFloat(apr.value) /100 /12
     var payements = parseFloat(years.value) * 12
 
-    var x = math.pow(1+ interest,, payements)
+    var x = math.pow(1+ interest, payements)
     var monthly = (principal*x*interest) / (x-1)
 
     if (isFinite(monthly)){
@@ -76,4 +77,78 @@ function calcular(){
         }
     }
     
+    function chart(principal, interest, monthly, payements){
+        var graph = document.getElementById("graph");
+        graph.width = graph.width
+
+        if(aruments.length == 0 || !graph.getContext) return;
+
+        var g = graph.getContext("2d");
+        var width = graph.width, height = graph.height;
+
+        function paymentToX(n){ return n * width / payements}
+        function amountToY(a) { return height - (a * height/(monthly*payements))}
+        g.moveTo(paymentToX(0), amountToY(0));
+        lineTo(paymentToX(payments),	
+            amountToY(monthly*payments));
+        g.lineTo(paymentToX(payments), amountToY(0));
+        g.closePath();
+        g.fillStyle ="#f88"
+        g.fill();
+        g.font = "bold 12px sans-serif";
+        g.fillText("Total interest in paymets",20,20)
+
+        var equity = 0;
+        g.beginPath();
+        g.moveTo(paymentToX(0), amountToY(0));
+
+        for(var p = 1; p <= payments; p++){
+            var ThisMonthsIntrest = (principal-equity)*interest
+            equity += (monthly - ThisMonthsIntrest)
+            g.lineTo(paymentToX(p), amountToY(equity));
+        }
+
+        g.lineTo(paymentToX(paymests), amountToY(0));
+        g.closePath();
+        g.fillStyle = "green";
+        g.fill();
+        g.fillText("Total equity", 20,35)
+
+        var bal = principal;
+        g.beginPath();
+        g.moveTo(paymentToX(0), amountToY(bal))
+        for(var p = 1; p <= payments; p++){
+            var ThisMonthsIntrest = bal*interest
+            bal -= (monthly - ThisMonthsIntrest)
+            g.lineTo(paymentToX(p), amountToY(bal))
+
+        }
+
+        g.lineWidth = 3;
+        g.stroke();
+        g.fillStyle = 'black'
+        g.fillText('Loan balance', 20,50)
+
+        g.textAlign='center';
+
+        var y = amountToY(0)
+        for(var year=1; year*12 <= payments; year++){
+            var x = paymentToX(year*12)
+            g.fillRect(x-0.5, y-3,1,3)
+            if(year == 1) g.fillText('Year', x,y-5)
+            if(year%5 == 0 && year* 12 !== payments)
+                g.fillText(String(year), x,y-5)
+        }
+
+        g.textAlign = 'right';
+        g.textBaseline = 'middle';
+        var ticks = [monthly*payments,principal]
+        var rightEdge = paymentToX(payements)
+        for(var i = 0; i < ticks.length; i++){
+            var y = amountToY(ticks[i].toFixed(0),
+                rightEdge-5, y)
+        }
+
+        
+    }
 }
---
+++ JAVASCRIPT/[RocketSeat]Node JS/PROJETOS/001/02.js
@@ -0,0 +1,4 @@
+const getFlagValue = require('./01')
+
+
+console.log(`Oi ${getFlagValue('--name')}. ${getFlagValue('--greting')}`)
